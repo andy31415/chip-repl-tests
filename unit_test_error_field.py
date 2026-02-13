7e26cfe357746a11f278f46c8005450ca0a08ae2
Andrei Litvin <andy314@gmail.com>
2026-02-13 12:15:41 -0500

    Add extra file

---
+++ unit_test_error_field.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import asyncio
+import chipstart
+import click
+import pprint
+
+import chip.clusters as Clusters
+from chip.ChipStack import ChipStack
+
+
+async def commission_impl(devCtrl, node_id):
+    await devCtrl.CommissionOnNetwork(node_id, 20202021)
+    devCtrl.Shutdown()
+
+
+async def read_failure_impl(devCtrl, node_id):
+    attr = await devCtrl.ReadAttribute(
+        node_id, [(1, Clusters.UnitTesting.Attributes.FailureInt32U)]
+    )
+    pprint.pprint(attr)
+    devCtrl.Shutdown()
+
+
+@chipstart.main.command()
+@click.pass_context
+@click.option("--node-id", "-n", default=1234, show_default=True)
+def commission(ctx, node_id):
+    asyncio.get_event_loop().run_until_complete(
+        commission_impl(ctx.obj["devCtrl"], node_id)
+    )
+
+
+@chipstart.main.command()
+@click.pass_context
+@click.option("--node-id", "-n", default=1234, show_default=True)
+def read_failure(ctx, node_id):
+    asyncio.get_event_loop().run_until_complete(
+        read_failure_impl(ctx.obj["devCtrl"], node_id)
+    )
+
+
+if __name__ == "__main__":
+    chipstart.main()
+    ChipStack.Shutdown()
